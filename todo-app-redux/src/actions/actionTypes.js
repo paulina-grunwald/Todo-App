6d9309f69acb214ad227fb3fbdf55f61b59e5bc2
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-10-25 10:35:16 +0200

    create file with constants that will be used with action creators and reducers

---
+++ todo-app-redux/src/actions/actionTypes.js
@@ -0,0 +1,8 @@
+//create the constants we gonna use on the actions creators, reducers
+export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const REMOVE_TODO = 'REMOVE_TODO'
+export const SHOW_ALL = 'SHOW_ALL'
+export const SHOW_COMPLETED = 'SHOW_COMPLETED'
+export const SHOW_ACTIVE = 'SHOW_ACTIVE'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
